ec7abb70c623f1bd9abb70678a478e42141676b4
L1d1a <lidia.wolinska1@gmail.com>
2024-10-03 12:15:41 +0100

    Initial comm

---
+++ src/app/devices/devices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-devices',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './devices.component.html',
+  styleUrls: ['./devices.component.css']
+})
+export class DevicesComponent { }
